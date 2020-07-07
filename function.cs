17a37697ab9f1b7d14ab3dca5af6c9e9255a1784
Hermer29 <kliman.chan@gmail.com>
2020-07-07 10:23:42 +0300

    Test branch commit

---
+++ function.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Project
+{
+    class Feature
+    {
+        public static void Print(string Text)
+        {
+            Console.WriteLine(Text);
+        }
+    }
+}
---
+++ name.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Project
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Feature.Print("Hello World!");
+        }
+    }
+}
